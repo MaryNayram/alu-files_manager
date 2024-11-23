33d01ad0afb8e85c63e642c6d1ee2ae8e9bb3ce1
Mary <122350784+MaryNayram@users.noreply.github.com>
2024-11-23 22:18:55 +0000

    All done

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+module.exports = {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          targets: {
+            node: 'current',
+          },
+        },
+      ],
+    ],
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable eol-last */
+/* eslint-disable linebreak-style */
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 5000;
+const routes = require('./routes/index');
+
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ extended: false, limit: '20mb' }));
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
